b5cd4552d8c0335a34979c6dfe8a295aa22662a4
Piotr Macuk <piotr@macuk.pl>
2015-08-20 11:29:41 +0200

    Static html from react

---
+++ star.js
@@ -0,0 +1,13 @@
+import Icon from 'react-fa'
+
+class Star extends React.Component {
+  render () {
+    return (
+      <a>
+        <Icon name='star-o' />
+      </a>
+    )
+  }
+}
+
+export default Star
