50732faf5d75b4a1cafe077d1310dd285b683b2d
Piotr Macuk <piotr@macuk.pl>
2015-08-20 11:47:07 +0200

    Handle 2s break between clicking

--- star.js
+++ star.js
@@ -3,15 +3,30 @@ import Icon from 'react-fa'
 class Star extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { label: 'star-o' };
+    this.state = { label: 'star-o', lastClickedTimeoutId: null };
     this.labelClicked = this.labelClicked.bind(this);
+    this.labelClicked2SecondsAgo = this.labelClicked2SecondsAgo.bind(this);
   }
 
   labelClicked() {
+    const currentLabel  = this.state.label;
     const currentLabelIndex = this.props.labels.indexOf(this.state.label);
-    const nextLabelIndex = (currentLabelIndex+1) % this.props.labels.length;
+    let nextLabelIndex;
+    if (currentLabelIndex != 0 && !this.state.lastClickedTimeoutId) {
+      nextLabelIndex = 0;
+    } else {
+      nextLabelIndex = (currentLabelIndex+1) % this.props.labels.length;
+    }
     const nextLabel = this.props.labels[nextLabelIndex];
-    this.setState({ label: nextLabel });
+    
+    clearTimeout(this.state.lastClickedTimeoutId);
+    const timeoutId = setTimeout(this.labelClicked2SecondsAgo, 2000);
+
+    this.setState({ label: nextLabel, lastClickedTimeoutId: timeoutId });
+  }
+
+  labelClicked2SecondsAgo() {
+    this.setState({lastClickedTimeoutId: null});
   }
 
   render () {
