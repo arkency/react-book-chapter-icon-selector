fac4c18128b981a9ab2f5f33bb6c0277b7a9f5aa
Piotr Macuk <piotr@macuk.pl>
2015-08-20 11:33:07 +0200

    Clickable icon

--- star.js
+++ star.js
@@ -4,11 +4,16 @@ class Star extends React.Component {
   constructor(props) {
     super(props);
     this.state = { label: 'star-o' };
+    this.labelClicked = this.labelClicked.bind(this);
+  }
+
+  labelClicked(){
+    this.setState({ label: 'star' });
   }
 
   render () {
     return (
-      <a>
+      <a onClick={this.labelClicked} >
         <Icon name={this.state.label} />
       </a>
     )
